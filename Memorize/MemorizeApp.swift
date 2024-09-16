b0ccced5f97611e34047522f4854e9501d8b1719
Faheem Ahmed <faheemahmed5525@gmail.com>
2024-09-16 15:06:31 +0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Faheeam Ahmed on 16/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
